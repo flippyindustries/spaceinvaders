fadc7ee17c0d10a8c9f444e94daece5c9532931c
Eric Tavela <etavela@5amsolutions.com>
2018-06-25 05:52:55 -0400

    Revised game loop sleep approach

--- src/main/java/com/alevat/spaceinvaders/game/GameLoop.java
+++ src/main/java/com/alevat/spaceinvaders/game/GameLoop.java
@@ -1,36 +1,70 @@
 package com.alevat.spaceinvaders.game;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import static java.lang.System.nanoTime;
+
 class GameLoop implements Runnable {
 
     private final static int TARGET_FPS = 60;
+    private final static long TARGET_NANOSECONDS_PER_FRAME = TimeUnit.SECONDS.toNanos(1) / TARGET_FPS;
 
     private final Game game;
     private boolean running;
+    private long frameCount = 0;
+    private long startTimeNanos;
+    private long cycleStartTimeNanos;
 
     GameLoop(Game game) {
         this.game = game;
     }
 
     void start() {
-        new Thread(this).start();
+        Thread thread = new Thread(this);
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.start();
     }
 
     @Override
     public void run() {
         running = true;
+        startTimeNanos = nanoTime();
         while (running) {
             executeGameCycle();
         }
     }
 
     private void executeGameCycle() {
-        try {
-            getGameState().update();
-            getScreen().render();
-            Thread.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        cycleStartTimeNanos = nanoTime();
+        update();
+        render();
+        sleep();
+        frameCount++;
+        if (frameCount % 100 == 0) {
+            getConsole().info("Average FPS: " + getAverageFPS());
         }
+    }
+
+    private double getAverageFPS() {
+        final long currentTimeNanos = nanoTime();
+        final long durationNanos = currentTimeNanos - startTimeNanos;
+        return (double) frameCount / TimeUnit.NANOSECONDS.toSeconds(durationNanos);
+    }
+
+    private void update() {
+        getGameState().update();
+    }
+
+    private void render() {
+        getScreen().render();
+    }
+
+    private void sleep() {
+        final long currentTimeNanos = nanoTime();
+        final long elapsedCycleTimeNanos = cycleStartTimeNanos - currentTimeNanos;
+        final long requestedSleepTimeNanos = TARGET_NANOSECONDS_PER_FRAME - elapsedCycleTimeNanos;
+        LockSupport.parkNanos(requestedSleepTimeNanos);
     }
 
     void quit() {
@@ -43,5 +77,9 @@ class GameLoop implements Runnable {
 
     private Screen getScreen() {
         return game.getScreen();
+    }
+
+    private Console getConsole() {
+        return game.getConsole();
     }
 }
