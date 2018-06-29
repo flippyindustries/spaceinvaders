282ce9ddc70381dafbaa36071f831610efd22ad2
Eric Tavela <etavela@5amsolutions.com>
2018-06-29 14:06:43 -0400

    Revised screen drawing approach

--- src/main/java/com/alevat/spaceinvaders/awt/JavaAWTRenderingEngine.java
+++ src/main/java/com/alevat/spaceinvaders/awt/JavaAWTRenderingEngine.java
@@ -3,21 +3,35 @@ package com.alevat.spaceinvaders.awt;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import javax.swing.*;
 
 import com.alevat.spaceinvaders.game.Console;
+import com.alevat.spaceinvaders.game.Screen;
+import com.alevat.spaceinvaders.io.ImageResource;
 import com.alevat.spaceinvaders.io.RenderingEngine;
 import com.alevat.spaceinvaders.io.Sprite;
+
+import static java.awt.Image.SCALE_SMOOTH;
 
 class JavaAWTRenderingEngine implements RenderingEngine {
 
     private static final int DISPLAY_MODE_HEIGHT = 300;
     private static final int DISPLAY_MODE_WIDTH = 400;
+    private final static int SCREEN_SIZE_MULTIPLIER = 4;
+
     private final Console console;
     private GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
     private GraphicsDevice graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
     private Frame frame;
-    private ScreenPanel screenPanel;
+    private final Set<Sprite> sprites = new HashSet<>();
+    private final Map<ImageResource, BufferedImage> resizedImageMap = new HashMap<>();
+    private BufferStrategy bufferStrategy;
+    private Rectangle frameBounds;
 
     JavaAWTRenderingEngine(Console console) {
         this.console = console;
@@ -26,37 +40,24 @@ class JavaAWTRenderingEngine implements RenderingEngine {
     @Override
     public void initializeScreen() {
         initializeFrame();
-        initializeScreenPanel();
         displayScreen();
     }
 
     private void initializeFrame() {
-        frame = new Frame();
+        frame = new Frame(graphicsDevice.getDefaultConfiguration());
         frame.setUndecorated(true);
         frame.setIgnoreRepaint(true);
     }
 
-    private void initializeScreenPanel() {
-        screenPanel = new ScreenPanel();
-        screenPanel.initialize();
-    }
-
     private void displayScreen() {
+        frame.requestFocus();
         graphicsDevice.setFullScreenWindow(frame);
         if (graphicsDevice.isDisplayChangeSupported()) {
             selectDisplayMode();
         }
-        Rectangle bounds = frame.getBounds();
-        frame.createBufferStrategy(3);
-        BufferStrategy bufferStrategy = frame.getBufferStrategy();
-        do {
-            do {
-                Graphics graphics = bufferStrategy.getDrawGraphics();
-                screenPanel.renderScreenImage(graphics);
-                graphics.dispose();
-            } while (bufferStrategy.contentsRestored());
-            bufferStrategy.show();
-        } while (bufferStrategy.contentsLost());
+        frameBounds = frame.getBounds();
+        frame.createBufferStrategy(2);
+        bufferStrategy = frame.getBufferStrategy();
     }
 
     private void selectDisplayMode() {
@@ -71,21 +72,75 @@ class JavaAWTRenderingEngine implements RenderingEngine {
 
     @Override
     public void addSprite(Sprite sprite) {
-        screenPanel.addSprite(sprite);
+        sprites.add(sprite);
     }
 
     @Override
     public void removeSprite(Sprite sprite) {
-        screenPanel.removeSprite(sprite);
+        sprites.remove(sprite);
     }
 
     @Override
     public void renderScreen() {
-        screenPanel.renderScreen();
+        do {
+            do {
+                Graphics graphics = bufferStrategy.getDrawGraphics();
+                graphics.setColor(Color.black);
+                graphics.fillRect(0, 0, (int) frameBounds.getWidth(), (int) frameBounds.getHeight());
+                renderScreenImage(graphics);
+                graphics.dispose();
+            } while (bufferStrategy.contentsRestored());
+            bufferStrategy.show();
+        } while (bufferStrategy.contentsLost());
     }
 
+    private void renderScreenImage(Graphics graphics) {
+        renderSprites(graphics);
+    }
+
+    private void renderSprites(Graphics graphics) {
+        for (Sprite sprite : sprites) {
+            renderSprite(sprite, graphics);
+        }
+    }
+
+    private void renderSprite(Sprite sprite, Graphics graphics) {
+        BufferedImage bufferedImage = getBufferedImage(sprite);
+        int x = getX(sprite);
+        int y = getY(sprite);
+        graphics.drawImage(bufferedImage, x, y, null);
+    }
+
+    private BufferedImage getBufferedImage(Sprite sprite) {
+        ImageResource imageResource = sprite.getImageResource();
+        BufferedImage resizedImage = resizedImageMap.get(imageResource);
+        if (resizedImage == null) {
+            resizedImage = getResizedBufferedImage(imageResource);
+            resizedImageMap.put(imageResource, resizedImage);
+        }
+        return resizedImage;
+    }
+
+    private BufferedImage getResizedBufferedImage(ImageResource imageResource) {
+        BufferedImage originalImage = imageResource.getBufferedImage();
+        int desiredWidth = SCREEN_SIZE_MULTIPLIER * originalImage.getWidth();
+        int desiredHeight = SCREEN_SIZE_MULTIPLIER * originalImage.getHeight();
+        Image tempResizedImage = originalImage.getScaledInstance(desiredWidth, desiredHeight, SCALE_SMOOTH);
+        BufferedImage resizedImage = new BufferedImage(desiredWidth, desiredHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics graphics = resizedImage.getGraphics();
+        graphics.drawImage(tempResizedImage, 0, 0, null);
+        return resizedImage;
+    }
+
+    private int getX(Sprite sprite) {
+        return sprite.getX() * SCREEN_SIZE_MULTIPLIER;
+    }
+
+    private int getY(Sprite sprite) {
+        return (Screen.HEIGHT - sprite.getY()) * SCREEN_SIZE_MULTIPLIER;
+    }
     void registerKeyListener(JavaAWTKeyListener keyListener) {
-        screenPanel.addKeyListener(keyListener);
+        frame.addKeyListener(keyListener);
     }
 
     void close() {
